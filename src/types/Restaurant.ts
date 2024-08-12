ea268067eb8bcee1f99c02a75083ab66abf14418
yuxnzs <yuchengliu0429@gmail.com>
2024-08-13 02:24:23 +0800

    Add Restaurant interface for backend data

---
+++ src/types/Restaurant.ts
@@ -0,0 +1,10 @@
+export default interface Restaurant {
+  id: number;
+  name: string;
+  image: string;
+  priceRange: string;
+  location: string;
+  rating: number;
+  category: string;
+  menuItemCount: number;
+}
