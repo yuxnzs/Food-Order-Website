9f6b69075fe7868a55f6f4ff68b788c11a6a6fa3
yuxnzs <yuchengliu0429@gmail.com>
2024-08-16 05:03:21 +0800

    Add routes for Homepage and RestaurantDetails page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { APIProvider } from "./services/APIContext";
 import Homepage from "./pages/Homepage";
+import RestaurantDetails from "./pages/RestaurantDetails";
 import "./styles/style.css";
 
 function App() {
   return (
     <APIProvider>
-      <Homepage />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route
+            path="/restaurant/:category/:id"
+            element={<RestaurantDetails />}
+          />
+        </Routes>
+      </Router>
     </APIProvider>
   );
 }
